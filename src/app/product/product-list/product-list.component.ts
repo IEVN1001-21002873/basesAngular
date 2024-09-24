2b66ffd3f435a21b8f704094af0d75dd6ab66206
Javier Lozano <javierlozano2515@gmail.com>
2024-09-23 20:37:18 -0600

    tercer commit lunes 23 filtros

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -10,10 +11,11 @@ export class ProductListComponent {
   imageWidth:number=50;
   imageMargin:number=2;
   muestraImg:boolean=true;
+  listFiter:string='';
   mostrarImagen():void{
   this.muestraImg=!this.muestraImg
   }
-productos:any[]=[
+productos:IProductos[]=[
   {
     "ProductoId":1,
     "Modelo":"Lambo",
@@ -21,7 +23,8 @@ productos:any[]=[
     "Year":"febrero 2 2020",
     "Precio":1200000,
     "Color":"Azul",
-    "ImagenUrl":"https://hdqwalls.com/wallpapers/dodge-challenger-srt-hellcat-widebody-2018-4k-3b.jpg"
+    "Marca":"Lamborghini",
+    "ImagenUrl":"https://media.autoexpress.co.uk/image/private/s--tGfOZghT--/v1562246756/autoexpress/2018/08/518274.jpg"
 },
 {
 "ProductoId":2,
@@ -30,6 +33,7 @@ productos:any[]=[
     "Year":"febrero 2 2020",
     "Precio":20000,
     "Color":"Azul",
+    "Marca":"Nissan",
     "ImagenUrl":"https://th.bing.com/th/id/R.c37fae810c8fb213b1ce1c8d2917d8fa?rik=lETrN6HNqflQtQ&pid=ImgRaw&r=0"
   },
   {
@@ -39,6 +43,7 @@ productos:any[]=[
     "Year":"febrero 2 2020",
     "Precio":20000,
     "Color":"Azul",
+    "Marca":"Audi",
     "ImagenUrl":"https://th.bing.com/th/id/R.aecb55fc4993ee4332a5ac147c3a1aa8?rik=h%2bTogZhlYuUuxQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2f9%2f97%2fAudi_A1.jpg&ehk=TjzJus6E3hT5EXCpzRdjWn5wx8ikVyffAZvb4VmHRlc%3d&risl=&pid=ImgRaw&r=0"
   }
 ]
---
+++ src/app/product/productos.ts
@@ -0,0 +1,10 @@
+export interface IProductos {
+    ProductoId:number;
+    Modelo:string;
+    Descripcion:string;
+    Year:string;
+    Precio:number;
+    Color:string;
+    Marca:string;
+    ImagenUrl:string;
+}
